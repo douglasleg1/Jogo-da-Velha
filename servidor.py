df840393c26524c1b06939ead48aa5bc51648164
Douglas Dantas de Souza <doug.souda@gmail.com>
2016-10-18 15:23:15 -0300

    Realocando servidor

--- servidor.py
+++ servidor.py
@@ -1,29 +1,29 @@
 # Servidor
 
 import socket
-print "Servidor"
+print("Servidor")
 
-HOST = "localhost"
+HOST =("localhost")
 PORT = 57000
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "Escutando a porta..."
+print("Escutando a porta...")
 s.bind((HOST,PORT))
 s.listen(1)
 
-print "Aceitando a conexao..."
+print("Aceitando a conexao...")
 conn,addr= s.accept()
 
-print "recebendo o arquivo..."
+print("recebendo o arquivo...")
 arq = open('File_ouputt.rar','wb')
 
 while 1:
 
-dados=conn.recv(1024)
-if not dados:
-	break
-arq.write(dados)
+        dados=conn.recv(1024)
+        if not dados:
+                break
+        arq.write(dados)
 
-print "saindo..."
+        print("saindo...")
 arq.close()
 conn.close()
